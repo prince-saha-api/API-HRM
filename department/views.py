cd3c5aab057763259b42eb9927564e666a44db58
Sathy77 <sathy754@gmail.com>
2024-05-22 11:40:17 +0600

    changes

--- department/views.py
+++ department/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
-# from helps.decorators.decorator import CommonDecorator as deco
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from department import models as MODELS_D
-from department.serializer import serializers as SRLZER_D
-from rest_framework.response import Response
-from rest_framework import status
-from helps.common.generic import Generichelps as ghelp
 
 # Create your views here.
