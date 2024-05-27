04738927a3c55d1f17843e4abe52b3c805662b4a
Sathy77 <sathy754@gmail.com>
2024-05-27 11:52:14 +0600

    my changes

--- hrm_settings/serializer/serializers.py
+++ hrm_settings/serializer/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from hrm_settings import models
+
+class Fiscalyearserializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Fiscalyear
+        fields = '__all__'
--- hrm_settings/views.py
+++ hrm_settings/views.py
@@ -1,1 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from hrm_settings import models as MODELS_SETT
+from hrm_settings.serializer import serializers as SRLZER_SETT
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# @deco.get_permission(['Get Single Permission Details', 'all'])
+def getfiscalyears(request):
+    fiscalyears = MODELS_SETT.Fiscalyear.objects.all()
+    fiscalyearserializers = SRLZER_SETT.Fiscalyearserializer(fiscalyears, many=True)
+    return Response(fiscalyearserializers.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+def addfiscalyear(request):
+    fiscalyearserializers = SRLZER_SETT.Fiscalyearserializer(data=request.data, many=False)
+    if fiscalyearserializers.is_valid():
+        fiscalyearserializers.save()
+        return Response({'data': fiscalyearserializers.data}, status=status.HTTP_201_CREATED)
+    else: return Response({'data': []}, status=status.HTTP_400_BAD_REQUEST)
